fca5b450dbd434a510e85b7ba1395a3c2a1a9077
Vasilis Moutzouridis <vasilismoutzouridis@Vasiliss-MacBook-Pro.local>
2025-02-25 12:10:33 +1100

    Make it listen to outside requests

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.use(generic);
 scheduleProjectsJob()
 
 let PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => { 
   console.log(`Server is running on port ${PORT}`);
-})
+});
